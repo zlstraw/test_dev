3b16d4cd191e84ac843b0025244a6dd5ef78d71e
zlstraw <2861503599@qq.com>
2018-09-30 14:54:02 +0800

    update

--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import auth
 # Create your views here.
 
 # 主要代码逻辑
@@ -8,18 +9,28 @@ def index(request):
 
 #处理登陆请求
 def login_action(request):
-    if request.method == "GET":
-        username = request.GET.get("username")
-        password = request.GET.get("password")
+    if request.method == "POST":
+        username = request.POST.get("username","")
+        password = request.POST.get("password","")
+
 
         if username == "" or password == "":
             return render(request, "index.html",
-                          {"error":"用户名或者密码错误"}
+                          {"error":"用户名或者密码null"}
                           )
+        else:
+            user = auth.authenticate(username=username,
+                                     password=password)
+            if user is not None:
+                auth.login(request,user) #验证登陆
+                return render(request,"project_manage.html")
+            else:
+                return render(request,"index.html",
+                              {"error":"用户名或密码错误"})
 
-"""
-gjango的开发模式
-M   models.py
-T   templates ->HTML
-V   views.py
-"""
+        #if username == "admin" and password =="123456":
+         #   return render(request,"project_manage.html")
+        #else:
+         #   return render(request,"index.html",
+          #                {"error":"用户名或密码错误"})
+
