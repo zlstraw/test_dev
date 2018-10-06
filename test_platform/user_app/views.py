2c132f3b94349958ca9c2a37f04c20d11e81bffb
zlstraw <2861503599@qq.com>
2018-10-06 15:15:36 +0800

    update

--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
-# 主要代码逻辑
+# 首页
 def index(request):
     return render(request,"index.html")
 
@@ -15,22 +15,27 @@ def login_action(request):
 
 
         if username == "" or password == "":
-            return render(request, "index.html",
-                          {"error":"用户名或者密码null"}
-                          )
+            return render(request, "index.html",{"error":"用户名或者密码null"})
         else:
-            user = auth.authenticate(username=username,
-                                     password=password)
+            # 验证用户是否存在
+            user = auth.authenticate(username=username,password=password)
+
             if user is not None:
-                auth.login(request,user) #验证登陆
-                return render(request,"project_manage.html")
+                # 记录用户登陆状态
+                auth.login(request,user)
+                request.session['user1'] = username
+                return HttpResponseRedirect('/project_manage/')
             else:
-                return render(request,"index.html",
-                              {"error":"用户名或密码错误"})
+                return render(request,"index.html",{"error":"用户名或密码错误"})
 
-        #if username == "admin" and password =="123456":
-         #   return render(request,"project_manage.html")
-        #else:
-         #   return render(request,"index.html",
-          #                {"error":"用户名或密码错误"})
+# 判断用户是否登陆
+@login_required
+def project_manage(request):
+    username = request.session.get('user1','')  # 读取浏览器session
+    return render(request,"project_manage.html",{"user": username})
 
+# 退出登录
+def logout(request):
+    auth.logout(request)   # 清除用户的登陆状态
+    response = HttpResponseRedirect('/')
+    return response
